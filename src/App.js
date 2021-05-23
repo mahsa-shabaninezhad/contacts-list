b4e925200ddfcc910912797012d55e5364254353
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-23 16:15:52 +0430

    usign Contact, ContactDetails and SideBar in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+import ContactDetails from './components/ContactDetails/ContactDetails';
+import Contact from './components/Contact/Contact';
+import SideBar from './components/SideBar/SideBar';
+import data from './data'
 
 function App() {
+  const [openContact,setOpenContact] = useState(false);
+  
+  const handleOpeningContact=(id) => {
+    setOpenContact(prevState => {
+      for (const person of data) {
+        if (person.id === id) return person
+    }})
+  }
+
+  const handleClosingContact = () => {
+    setOpenContact(false)
+  }
+
+  const contactList = data.map(person => <Contact key={person.id} contact={person} onClick={handleOpeningContact} />)
   return (
     <div className="App">
-      
+      <SideBar 
+        name="John Doe" 
+        email='johndoe@email.com'  
+        code='1904'
+        contactsNum='38'
+        partnersNum='14'
+      />
+      <div className="contactList">
+        {contactList}
+      </div>
+      {openContact && <ContactDetails contactInfo={openContact} close={handleClosingContact}/>}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,84 @@
+import person1 from './images/67.jpg'
+import person2 from './images/54.jpg'
+import person3 from './images/91.jpg'
+import person4 from './images/60.jpg'
+import person5 from './images/10.jpg'
+import person6 from './images/88.jpg'
+import person7 from './images/81.jpg'
+import person8 from './images/49.jpg'
+const data = [
+    {
+        id: 1,
+        name: 'Toni Lewis',
+        email: 'toni.lewis@example.com',
+        phone: '(669)-352-2850',
+        img: person1,
+        birthday: '7/2/1955',
+        address: '9020 Paddock Way',
+    },
+    {
+        id: 2,
+        name: 'corey mitchell',
+        email: 'corey.mitchell@example.com',
+        phone: '(624)-444-3877',
+        img: person6,
+        birthday: '8/5/1977',
+        address: '5374 Plum St',
+    },
+    {
+        id: 3,
+        name: 'Priscilla Wallace',
+        email: 'priscilla.wallace@example.com',
+        phone: '(083)-959-6064',
+        img: person3,
+        birthday: '5/1/1968',
+        address: '9693 Poplar Dr',
+    },
+    {
+        id: 4,
+        name: 'Gina Taylor',
+        email: 'gina.taylor@example.com',
+        phone: '(865)-091-7190',
+        img: person4,
+        birthday: '7/2/1990',
+        address: '5548 Nowlin Rd',
+    },
+    {
+        id: 5,
+        name: 'Dean Hudson',
+        email: 'dean.hudson@example.com',
+        phone: '(710)-607-7584',
+        img: person5,
+        birthday: '3/5/1992',
+        address: '7992 Wheeler Ridge Dr',
+    },
+    {
+        id: 6,
+        name: 'Annie Gardner',
+        email: 'annie.gardner@example.com',
+        phone: '(862)-232-6749',
+        img: person2,
+        birthday: '3/6/1960',
+        address: '2389 E Center St',
+    },
+    {
+        id: 7,
+        name: 'Phillip Wilson',
+        email: 'phillip.wilson@example.com',
+        phone: '(119)-930-6021',
+        img: person7,
+        birthday: '12/1/1972',
+        address: '2178 Rolling Green Rd',
+    },
+    {
+        id: 8,
+        name: 'gene foster',
+        email: 'gene.foster@example.com',
+        phone: '(860)-708-1806',
+        img: person8,
+        birthday: '8/7/1987',
+        address: '5791 Elgin St',
+    }
+]
+
+export default data;
